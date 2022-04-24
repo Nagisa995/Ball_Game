2d40dbc3fc8a5cabc0dd6c0d38da6c7052ed4761
Nagisa <miku195@mail.ru>
2022-04-24 22:40:05 +0700

    add jump function

--- js/main.js
+++ js/main.js
@@ -3,30 +3,53 @@ const ballUI = {
     axesX: 0,
     axesY: 0,
     accelerationX: 0,
-    accelerationY: 0
+    accelerationY: 0,
+    ground: 0
 };
 
 document.body.addEventListener('keydown', addAcceleration);
 
 function addAcceleration(event) {
-    console.log(event.code)
     switch (event.code) {
-        case ('KeyD'): ballMove(0.5);
+        case ('KeyD'): ballUI.accelerationX = 0.25;
             break;
-        case ('Space'):
+        case ('Space'): if (ballUI.axesY == ballUI.ground) ballUI.accelerationY = 4.9;
             break;
-        case ('KeyA'): ballMove(-0.5);
+        case ('KeyA'): ballUI.accelerationX = -0.25;
+            break;
+        default: return;
+    }
+}
+
+document.body.addEventListener('keyup', resetAcceleration);
+
+function resetAcceleration(event) {
+    switch (event.code) {
+        case ('KeyD'): if (ballUI.accelerationX > 0) ballUI.accelerationX = 0;
+            break;
+        case ('KeyA'): if (ballUI.accelerationX < 0) ballUI.accelerationX = 0;
             break;
         default: return;
     }
 }
 
 function ballJump() {
+    if (ballUI.accelerationY >= 2.5) {
+        ballUI.axesY += 0.5;
+        ballUI.accelerationY -= 0.1;
+    } else {
+        ballUI.axesY -= 0.5;
+        ballUI.accelerationY -= 0.5;
+    }
 
+    if (ballUI.axesY < 0) {
+        ballUI.axesY = 0;
+    }
 }
 
-function ballMove(acceleration) {
-    ballUI.axesX += acceleration;
+function ballMove() {
+    ballUI.axesX += ballUI.accelerationX;
+
     if (ballUI.axesX > 95) {
         ballUI.axesX = 95;
     } else if (ballUI.axesX < 0) {
@@ -35,6 +58,8 @@ function ballMove(acceleration) {
 }
 
 function ballOnUI() {
+    ballMove()
+    ballJump()
     ballUI.idOnUI.setAttribute('style', `bottom: ${ballUI.axesY}%; left: ${ballUI.axesX}%`);
 }
 
